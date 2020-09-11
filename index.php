6c1a12850a5f04cac60ba52dd246f9e79bad66a9
Shatakshi Sharma <sshatakshi53@gmail.com>
2020-09-11 14:27:01 +0530

    created

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>first task</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="Assets/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<!--navbar-->
+	<nav class="navbar navbar1">
+		<!--container for wrapping whole content in a desired width-->
+		<div class="container">
+			<!--left DD brandname-->
+			<div class="navbar-header">
+				<a class="navbar-brand navbar2" href="#"><b>DD</b></a>
+			</div>
+			<!--list of items residing in navbar-->
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item"><a href="#">PRODUCTEN</a></li>
+				<li class="nav-item"><a href="#">ABONERREN</a></li>
+				<li class="nav-item"><a href="#">OVER ONS</a></li>
+				<li class="nav-item"><a href="#">VAGEN</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!--card section for covering the middle section of body-->
+	<section class="bbody">
+		<!-- second container for wrapping whole content inside a fixed width-->
+		<div class="container">
+			<!--blue div at the right-->
+
+			<div class="nonusa-div">
+				<img class="cart-img" src="images\images\shopping-cart.png">
+				<p class="p1">29.96 (2)</p>
+			</div><br><br><br>
+			<!--centred paragraph-->
+			<div class="row">
+				<div class="p2 col-md-12 text-center">
+					Sneg el voordelig bestellen<br>Lorem ipsum dollar sit amet,consectatur edipicing elit.<br>Maecenas aliquer feugiet odio at mollis.
+				</div>
+			</div><br><br>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-12">
+						<div class="cardproduct">
+							<div class="">
+								<h2 class="text-center text-success h2_1">Abonneren</h2>
+								<hr>
+								<p class="p3">Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Nullam dapibus fermentum ipsum. Etiam quis quam. Integer lacinia. Nulla est. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat.</p>
+								<button class="btn1 btn btn-primary">MEER INFO</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-8 col-sm-12">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="left_div">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="cardproduct">
+									<div class="outerdiv">
+										<div class="bluediv">
+											<h5 class="text-center">Nutrilon<br>Pronultrat</h5>
+										</div>
+										<img src="images\images\website_list_page\package.png" class="img3" />
+										<br>
+										<button class="btn2 btn btn-success">KOPEN</button>
+										<br>
+										<button class="btn3 btn btn-primary">ABONNEREN</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+			<!--leftmost div
+			<div class="row">
+				<div class="col-md-4">
+					<div class="left_div">
+						<h2 class="text-center text-success h2_1">Abonneren</h2>
+						<hr>
+						<p class="p3">Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Nullam dapibus fermentum ipsum. Etiam quis quam. Integer lacinia. Nulla est. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat.</p>
+						<button class="btn1">MEER INFO</button>
+					</div>
+				</div>	
+				<div class="col-md-8">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card outerdiv">
+								<div class="card-body">
+									<div class="bluediv">
+										<h5>Nutrilon<br>Pronultrat</h5>
+									</div>
+									<img src="images\images\website_list_page\package.png" class="img3" />
+									<br>
+									<button class="btn2">KOPEN</button>
+									<br>
+									<button class="btn3">ABONNEREN</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>-->
+		</div>
+
+	</section>
+	<!-- Footer -->
+	<div style="background-color: beige;color:#596e79;"> 
+		<div class="container text-center" style="background-color: beige;color:#596e79;">
+			<div class="row" style="margin-top: 10px;">
+				<div class="col-md-4 ">
+					<ul class="list-unstyled"><li class="mb-3"><b><h3 class="text-left ddhead" style="margin-top: 0px;">DD</h3></b></li></ul>	
+				</div>
+				<div class="col-md-2 text-center">
+					<ul class="list-unstyled"><li class="mb-3 contact">Contact</li></ul>
+				</div>
+				<div class="col-md-2 text-center">
+					<ul class="list-unstyled  ">
+						<li class="mb-3 ">Tel</li>
+						<li class="mb-3 ">123-456-7890</li><br>
+						<li class="mb-3 ">Address.</li>
+						<li class="mb-3 ">Kavi Nagar,<br>Ghaziabad</li>
+					</ul>
+				</div>
+				<div class="col-md-2 text-center">
+					<ul class="list-unstyled ">
+						<li class="mb-3 ">E-mail.</li>
+						<li class="mb-3 ">abc@gmail.com</li><br>
+						<li class="mb-3 ">Website</li>
+						<li class="mb-3 ">abc.com</li>
+					</ul>
+				</div>
+				<div class="col-md-2 text-center">
+					<ul class="list-unstyled ">
+						<li class="mb-3">Siteamp.</li>
+						<li class="mb-3 ">PRODUCTEN</li>
+						<li class="mb-3 ">ABONNEREN</li>
+						<li class="mb-3 ">OVER ONS</li>
+						<li class="mb-3 ">VAGEN</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--script-->
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<script src="Assets/js/bootstrap.min.js"></script>
+</body>
+</html>
